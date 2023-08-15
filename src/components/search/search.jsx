c4252aa2f4cf9eea8bdf4f3600d670c74c6ea47f
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-08-15 10:50:48 +0300

    update

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -9,7 +9,7 @@ function Search({ setImages }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    if (searchTerm.trim() === "") {
+    if (searchTerm === "") {
       fetchRandom();
     } else {
       fetchSearchResults();
