b78928073118ee50b606cc48bdb8a851930a328b
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-08-16 17:42:41 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className={
       darkMode
-        ? "flex w-full h-full flex-col items-center justify-start p-14 transition duration-500 bg-black"
-        : "flex w-full flex-col items-center justify-start p-14 transition duration-500 bg-white"
+        ? "flex w-full h-full flex-col items-center justify-start max-sm:p-6 p-14 transition duration-500 bg-black"
+        : "flex w-full flex-col items-center justify-start max-sm:p-6 p-14 transition duration-500 bg-white"
     }>
       <ImageDisplay />
     </div>
